8412f809468fa1fc20fdc50aab94c5960e5d68c3
Gabriel Ramos de Sousa <gabrielramos.email@gmail.com>
2018-03-06 11:00:14 -0300

    Atualizando o repositório com pequenas correções

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,28 @@
-var idade = $(".idade");
-var aviso = $(".aviso");
+var frase = $(".frase").text();
+var numPalavras = frase.split(" ").length;
 
-idade.on("input", function(){
-  if (idade.val() >= 18){
-    aviso.text("Você é maior de idade")
-  }else {
-    aviso.text("Você é menor de idade")
-  }
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function() {
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function() {
+    var cronometroID = setInterval(function() {
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+        if (tempoRestante < 1) {
+            campo.attr("disabled", true);
+            clearInterval(cronometroID);
+        }
+    }, 1000);
 });
